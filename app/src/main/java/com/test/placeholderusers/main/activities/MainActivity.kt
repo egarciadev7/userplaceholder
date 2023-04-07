56ad86221c63d56453821aa26720de70f5da02bb
eduargarciam <eduar.urgeli@sweatworks.net>
2023-04-06 19:12:01 -0500

    added back button

--- app/src/main/java/com/test/placeholderusers/main/activities/MainActivity.kt
+++ app/src/main/java/com/test/placeholderusers/main/activities/MainActivity.kt
@@ -2,8 +2,11 @@ package com.test.placeholderusers.main.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import com.test.placeholderusers.R
 import com.test.placeholderusers.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -12,12 +15,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         setContentView(binding.root)
+        initListeners()
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressedDispatcher.onBackPressed()
         return true
     }
+
+    private fun initListeners() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_container_fragment) as NavHostFragment?
+        val navController = navHostFragment!!.navController
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            supportActionBar?.setDisplayHomeAsUpEnabled(destination.id != R.id.userListFragment)
+        }
+    }
 }
+
